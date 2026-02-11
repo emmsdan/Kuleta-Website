00485d2810f8d5f68cfd88685f41cf674ee92fde
Emmanuel Daniel <emmsdan.inc@gmail.com>
2026-02-11 19:43:06 +0100

    Fix build and image path errors

--- src/pages/AboutUsPage.tsx
+++ src/pages/AboutUsPage.tsx
@@ -1,7 +1,7 @@
 import { ImageWithFallback } from "@/app/components/figma/ImageWithFallback";
 import { Target, Heart, Globe, Key, Clock, DollarSign } from "lucide-react";
-import mandaImage from "@/asset/c3fb5af5eb9b460f4206a2cc93c7b7f159a5893c.png";
-import judithImage from "@/asset/0781dbd8f8e47b7b7a2635ca5190583ab69776e2.png";
+import mandaImage from "@/assets/c3fb5af5eb9b460f4206a2cc93c7b7f159a5893c.png";
+import judithImage from "@/assets/0781dbd8f8e47b7b7a2635ca5190583ab69776e2.png";
 
 export function AboutUsPage() {
   const values = [
