87ee1f43d01490fbd1d56b452a4898ff37b6541b
Emmanuel Daniel <emmsdan.inc@gmail.com>
2026-04-08 12:10:01 +0100

    Move commonent outside route

--- src/app/config.ts
+++
@@ -1,9 +0,0 @@
-export const KULETA_API_BASE_URL = "https://dev-preview-1025.kuleta.io/api/v2";
-export const KULETA_SHOP_BASE_URL = "https://dev-preview-1025.kuleta.io";
-
-export const SOCIAL_LINKS = {
-  instagram: process.env.NEXT_PUBLIC_INSTAGRAM_URL ?? "https://www.instagram.com/kuleta.io/",
-  linkedin: process.env.NEXT_PUBLIC_LINKEDIN_URL ?? "https://www.linkedin.com/company/kuleta/",
-};
-
-export const CONTACT_EMAIL = "info@kuleta.io";
